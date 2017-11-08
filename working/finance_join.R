cd6449f1a5266be6c5d1a6e87521162d1c98acb2
Will Doyle <wdoyle42@gmail.com>
2017-11-08 10:38:13 -0600

    Added in class work

--- working/finance_join.R
+++ working/finance_join.R
@@ -6,43 +6,69 @@ full_df<-NULL #Initialize empty dataset
 
 for (symbol in symbols){
 
+  # pull financials
 financial<-tq_get(symbol,get="financial",from="2001-01-01")
 
+# pull prices
 stock_price<-tq_get(symbol,get="stock.prices")
 
+# add name
 stock_price$name<-symbol
 
-#Just get income statements (IS) others are Balance Sheets (bs) and cash flows (CF)
-fin<-financial %>%
+#split day into year, month day
+
+stock_price$year<-year(stock_price$date)
+stock_price$month<-month(stock_price$date)
+stock_price$day<-day(stock_price$date)
+
+#Convert to average for the month, for merge with quarterly data
+stock_price<-stock_price%>%group_by(name,year,month)%>%
+  summarize_at(c("open","high","low","close","volume"),mean,na.rm=TRUE)
+
+# Get income statements (IS) others are Balance Sheets (bs) and cash flows (CF)
+fin_IS<-financial %>%
   filter(type == "IS") %>%
   select(quarter)%>%
   unnest()
 
-fin2<-financial%>%
+fin_BS<-financial%>%
   filter(type=="BS") %>%
   select(quarter) %>%
   unnest()
 
-fin<-rbind(fin,fin2)
+fin_CF<-financial%>%
+  filter(type=="CF") %>%
+  select(quarter) %>%
+  unnest()
 
+# combine all financials
+fin<-rbind(fin_IS,fin_BS,fin_CF)
+
+#add name of stock
 fin$name<-symbol
+# Get year for merge
+fin$year<-year(fin$date)
+# get month for merge
+fin$month<-month(fin$date)
 
-#quarterly
-q_df<-left_join(fin,stock_price,by=c("name","date"))
+#quarterly dataset from financials
+q_df<-left_join(fin,stock_price,by=c("name","year","month"))
 
+#Drop any missing
+q_df<-q_df%>%filter(!is.na(value))
+
+#Don't need group
+q_df<-q_df%>%select(-group)
 #Combine with full data frame
 full_df<-rbind(full_df,q_df)
 
 }
 
-g1<-ggplot(filter(q_df,category=="Revenue"),aes(x=value,y=close))
-g1<-g1+geom_point()
-g1
-
-#daily
-daily_df<-right_join(fin,stock_price,by=c("name"))
+## Use spread to create new column for all financial chars
+full_df2<-full_df%>%
+  spread(category,value)
 
 # linear model
 
-reg1<-lm(close~value,
-         data=filter(combined,category="Operating Revenues"))
+reg1<-lm(close~`Total Revenue`,data=full_df2);summary(reg1)
+         
